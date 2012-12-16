790c3a6cbe38d59ee5d80e0be7200da31a23c892
Steve Anton <anxuiz.nx@gmail.com>
2012-12-15 21:34:01 -0800

    Add some handy accessors and annotations to MethodMessageExecutor.

--- src/main/java/net/anxuiz/protobuf/packet/base/MethodMessageExecutor.java
+++ src/main/java/net/anxuiz/protobuf/packet/base/MethodMessageExecutor.java
@@ -3,6 +3,9 @@ package net.anxuiz.protobuf.packet.base;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import net.anxuiz.protobuf.packet.HandlerException;
 import net.anxuiz.protobuf.packet.MessageHandler;
 
@@ -10,18 +13,27 @@ import com.google.common.base.Preconditions;
 import com.google.protobuf.Message;
 
 public class MethodMessageExecutor implements MessageHandler {
-    private final Object parent;
-    private final Method method;
+    private final @Nullable Object parent;
+    private final @Nonnull Method method;
 
-    public MethodMessageExecutor(final Object parent, final Method method) {
-        Preconditions.checkNotNull(parent, "parent");
+    public MethodMessageExecutor(@Nullable Object parent, @Nonnull Method method) {
         Preconditions.checkNotNull(method, "method");
 
         this.parent = parent;
         this.method = method;
     }
 
-    public void handle(Message msg) throws HandlerException {
+    public @Nullable Object getParent() {
+        return this.parent;
+    }
+
+    public @Nonnull Method getMethod() {
+        return this.method;
+    }
+
+    public void handle(@Nonnull Message msg) throws HandlerException {
+        Preconditions.checkNotNull(msg, "message");
+
         try {
             this.method.invoke(this.parent, msg);
         } catch (IllegalAccessException e) {
